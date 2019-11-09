76632ac43b60f625154b46926e29329fe090f241
horokai <horokai@outlook.jp>
2019-11-09 17:53:38 +0900

    Temporary Save

---
+++ README.md
@@ -0,0 +1,13 @@
+# normal-square-chart
+For chart-action2 : basic chart action on square lattice
+
+## Interface
+
+struct Universe and the impl.
+
+- member : length, count, view_info
+- new -> Universe
+- get length, count and view_info
+- tick
+- reset
+- reverse : change the parameter without reset
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 fn main() {
     println!("Hello, world!");
 }
@@ -6,18 +5,20 @@ fn main() {
 pub struct Universe {
     length: u32,
     count: u32,
+    view_info: Vec<u8>,
 }
 
 impl Universe {
-
     pub fn countup(&mut self) {
         self.count += 1;
-    }    
+    }
 
     pub fn new(length: u32) -> Universe {
+        let view_info = vec![0u8; (length * length) as usize];
         Universe {
             length,
             count: 0u32,
+            view_info,
         }
     }
 
@@ -28,5 +29,22 @@ impl Universe {
     pub fn get_count(&self) -> u32 {
         self.count
     }
- 
+
+    pub fn get_view_info(&self) -> *const u8 {
+        self.view_info.as_ptr()
+    }
+
+    pub fn reset(&mut self, length: u32) {
+        self.length = length;
+        self.count = 0u32;
+        let size = self.view_info.len();
+        for i in 0..size {
+            self.view_info[i as usize] = 0;
+        }
+    }
+
+    pub fn reverse(&mut self) {
+        
+    }
+
 }
