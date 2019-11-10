6418a828eca44d1c57f56b1cfcb4122a7ae9bc76
horokai <horokai@outlook.jp>
2019-11-10 17:05:48 +0900

    wasm setting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,32 @@
 
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, chart-action2!");
+}
+
+#[wasm_bindgen]
 pub struct Universe {
     length: u32,
     count: u32,
     view_info: Vec<u8>,
 }
 
+#[wasm_bindgen]
 impl Universe {
     pub fn countup(&mut self) {
         self.count += 1;
